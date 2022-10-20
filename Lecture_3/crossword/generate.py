827fc2c6caab906b391136c9ec9821f72564e820
OttoMP <otto28mai@gmail.com>
2022-10-20 13:34:32 -0300

    consistent function done

--- Lecture_3/crossword/generate.py
+++ Lecture_3/crossword/generate.py
@@ -157,14 +157,30 @@ class CrosswordCreator():
         Return True if `assignment` is complete (i.e., assigns a value to each
         crossword variable); return False otherwise.
         """
-        raise NotImplementedError
+        values = {v for v in assignment.values}
+        return None not in values
 
     def consistent(self, assignment):
         """
         Return True if `assignment` is consistent (i.e., words fit in crossword
         puzzle without conflicting characters); return False otherwise.
         """
-        raise NotImplementedError
+        consistency = {(var, value) for (var, value) in assignment.items() if value is not None}
+        all_values = {value for (var, value) in consistency}
+        # Verifying repeated assignments
+        if len(all_values) != len(consistency):
+            return False
+        for var, word in consistency:
+            # Node consistency
+            if var.length != len(word):
+                return False
+            # Arc consistency
+            for neigh in self.crossword.neighbors(var):
+                if assignment[neigh] is not None:
+                    i,j = self.crossword.overlaps(var, neigh)
+                    if assignment[var][i] != assignment[neigh][j]:
+                        return False
+        return True
 
     def order_domain_values(self, var, assignment):
         """
